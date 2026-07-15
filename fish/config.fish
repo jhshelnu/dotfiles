b10848efd875df7143a72cd05727470a56144419
Harrison Shelnutt <harrison.shelnutt@agilebits.com>
2026-07-15 10:44:29 -0600

    improved gitignore syntax, added fish, jj, and herdr

---
+++ fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    set -g fish_greeting ''
+
+    set -gx EDITOR nvim
+
+    fish_add_path /opt/homebrew/bin 
+    fish_add_path /opt/homebrew/sbin
+    fish_add_path $HOME/.local/bin
+
+    fish_vi_key_bindings
+    bind -M insert \ca beginning-of-line # ctrl-a in insert mode
+    bind -M insert \ce end-of-line       # ctrl-e in insert mode
+
+    # pull in work-specific config if present
+    set -l work_config $__fish_config_dir/work.fish
+    test -f $work_config && source $work_config
+
+    command -q jj       && COMPLETE=fish jj        | source
+    command -q kubectl  && kubectl completion fish | source
+    command -q rustup   && rustup completions fish | source
+    command -q direnv   && direnv hook fish        | source
+    command -q starship && starship init fish      | source
+end
